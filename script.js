3d75eae7f0d50cbb6f8abcdcfd23a721224164ae
Alexandre Vallette <vallettea@gmail.com>
2014-11-30 14:47:58 +0100

    added real command

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ mopidy.on("event:trackPlaybackStarted", function (event) {
   cmd.kill('SIGHUP');
 
   // respawn
-  cmd = childProcess.spawn('tail', ['-l']);
+  cmd = childProcess.spawn("sox -t mp3 /usr/share/jukebox/media/" + uri + " -t wav - | sudo ~/PiFmRds/src/pi_fm_rds -audio -");
   
   cmd.stdout.on('data', function (data) {
     console.log('stdout: ' + data);
