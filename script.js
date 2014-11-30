b94334f54f4e63897bc64e01508bcc8ab423126f
Alexandre Vallette <vallettea@gmail.com>
2014-11-30 13:39:09 +0100

    is this teh event

--- script.js
+++ script.js
@@ -13,40 +13,9 @@ var mopidy = new Mopidy({
   webSocketUrl: 'ws://' + (process.env.HOST || 'localhost') + ':6680/mopidy/ws/'
 });
 
-mopidy.on(console.log.bind(console));
+// mopidy.on(console.log.bind(console));
 
-// mopidy.on("state:online", function () {
-//   accumulateTracksInDirs([], [null])
-//   .then(function(trackUris){
-//     return mopidy.library.search({ uris: trackUris }).then(function(results){
-//       console.log(utils.inspect(results[0].tracks));
-//     })
-//   })
-//   .then(process.exit.bind(process, 0));
-// }, logErrors);
+mopidy.on("event:trackPlaybackStarted", function (event) {
+  console.log(event.tl_track.uri)
+}, logErrors);
 
-// var getTracksFromRefs = getTypeFromRefs.bind(null, 'track');
-// var getDirsFromRefs = getTypeFromRefs.bind(null, 'directory');
-
-// function getTypeFromRefs(type, refs){
-//   return refs.filter(function(ref){
-//     return ref.type === type;
-//   })
-//   .map(function(ref){
-//     return ref.uri;
-//   });
-// }
-
-// function accumulateTracksInDirs(allTracks, dirs){
-//   return when.all(dirs.map(function(dir){ return mopidy.library.browse({ uri: dir }) }))
-//     .spread(function(results){
-//       if (!results){
-//         return allTracks;
-//       }
-
-//       var tracks = allTracks.concat(getTracksFromRefs(results));
-//       var dirs = getDirsFromRefs(results);
-
-//       return accumulateTracksInDirs(tracks, dirs);
-//     })
-// }
