ffabd68e9b243768431efd1c0f25e5d794b7f574
Alexandre Vallette <vallettea@gmail.com>
2014-11-30 18:41:37 +0100

    ok for wave

--- script.js
+++ script.js
@@ -28,22 +28,23 @@ mopidy.on("event:trackPlaybackStarted", function (event) {
   // respawn
   // var sox = childProcess.spawn("sox",["-t", "mp3", "/usr/share/jukebox/media/" + uri, "-t", "wav", "-"]);
   // var sox = childProcess.spawn("avconv", ["-i", "/usr/share/jukebox/media/" + uri, "-ac", "1", "-ar", "22050", "-b", "352k", "-f", "wav", "-"]);
-  var pifm = childProcess.spawn("/home/pi/PiFmRds/src/pi_fm_rds", ["-audio", "-"]);
+  // var pifm = childProcess.spawn("/home/pi/PiFmRds/src/pi_fm_rds", ["-audio", "-"]);
+  var pifm = childProcess.spawn("/home/pi/PiFmRds/src/pi_fm_rds", ["-audio", "/usr/share/jukebox/media/" + uri]);
 
-  sox.stdout.on('data', function (data) {
-    pifm.stdin.write(data);
-  });
+  // sox.stdout.on('data', function (data) {
+  //   pifm.stdin.write(data);
+  // });
 
-  sox.stderr.on('data', function (data) {
-    console.log('sox stderr: ' + data);
-  });
+  // sox.stderr.on('data', function (data) {
+  //   console.log('sox stderr: ' + data);
+  // });
 
-  sox.on('close', function (code) {
-    if (code !== 0) {
-      console.log('sox process exited with code ' + code);
-    }
-    pifm.stdin.end();
-  });
+  // sox.on('close', function (code) {
+  //   if (code !== 0) {
+  //     console.log('sox process exited with code ' + code);
+  //   }
+  //   pifm.stdin.end();
+  // });
   
   pifm.stdout.on('data', function (data) {
     console.log('stdout: ' + data);
