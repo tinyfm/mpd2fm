10bd263f10e91b748d1f2be6243ecb8ca651be6e
oncletom <hi@oncletom.io>
2014-12-06 14:19:25 +0000

    CLI help and initd script output.

--- README.md
+++ README.md
@@ -8,3 +8,31 @@ mpd2fm tracks *tinyfm* tracks and turn them into FM modulations.
 
 - a [Raspberry Pi with an antenna/metalic wire](http://makezine.com/projects/make-38-cameras-and-av/raspberry-pirate-radio/#steppers);
 - a [tinyfm sandbox](https://github.com/tinyfm/sandbox)-like Pi configuration;
+
+# Install
+
+```bash
+npm install -g git+https://github.com/tinyfm/mpd2fm.git
+```
+
+# Usage
+
+Use `mpd2fm --help` to display help about available commands.
+
+## init.d config
+
+Prints out the Debian `initd` config.
+
+```bash
+mpd2fm --initd-config
+```
+
+Pretty much handy to daemonize the command-line tool on a Raspberry Pi.
+
+## Playback events to FM
+
+Listens to tinyfm audio playback events (backed by [mopidy](https://www.mopidy.com/)) and broadcasts them as an FM signal.
+
+```
+mpd2fm --base-dir /usr/share/tinyfm --start
+```
---
+++ bin/cli.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var Mopidy = require('mopidy');
+var encoder = require('../lib/encoder');
+
+var argv = require('yargs')
+  .options('initd-config', {
+    alias: 'i',
+    boolean: true,
+    default: false,
+    description: 'Displays out the related Debian initd script.'
+  })
+  .options('base-dir', {
+    alias: 'd',
+    default: '/usr/share/tinyfm',
+    description: 'Base directory to resolve event media filepath from.'
+  })
+  .options('start', {
+    boolean: true,
+    default: false,
+    description: 'Starts to media playback to FM broadcasting server.'
+  })
+  .strict()
+  .argv;
+
+if (argv.initdConfig) {
+  return fs.createReadStream(path.join(__dirname, '..', 'dist', 'init.d', 'mpd2fm'))
+    .pipe(process.stdout);
+}
+
+if (argv.start) {
+  var mopidy = new Mopidy({
+    callingConvention: 'by-position-or-by-name',
+    webSocketUrl: 'ws://' + (process.env.HOST || 'localhost') + ':6680/mopidy/ws/'
+  });
+
+  var broadcast = encoder({ env: 'test' });
+  var currentStream;
+
+  mopidy.on("event:trackPlaybackStarted", function (event) {
+    // catch the uri
+    var filepath = event.tl_track.track.uri.split(":").pop();
+    console.log("-----------------------------------");
+    console.log("New song:", uri);
+
+    currentStream = broadcast(filepath);
+  }, logErrors);
+}
+
+
+function logErrors(err){
+  console.error(err);
+}
